330e23d86599edd07bba2c805964a6c50c8c52ec
Seth Guthrie <skguth@gmail.com>
2016-10-14 12:58:50 -0600

    Added lab4c files

---
+++ Lab4c/myisr.s
@@ -0,0 +1,57 @@
+resetISR:
+	push 	bp
+	mov 	bp, sp
+	call resetISRC
+
+tickISR:
+	push	ax
+	push	bx
+	push	cx
+	push	dx
+	push	si
+	push	di
+	push	bp
+	push	es
+	push	ds
+	sti
+
+	call	tickISRC
+	call	signalEOI
+	cli
+	pop		ds
+	pop		es
+	pop		bp
+	pop		di
+	pop		si
+	pop		dx
+	pop		cx
+	pop		bx
+	pop		ax
+	iret
+	
+
+kbISR:
+	push	ax
+	push	bx
+	push	cx
+	push	dx
+	push	si
+	push	di
+	push	bp
+	push	es
+	push	ds
+
+	sti
+	call	kbISRC
+	call	signalEOI
+	cli
+	pop		ds
+	pop		es
+	pop		bp
+	pop		di
+	pop		si
+	pop		dx
+	pop		cx
+	pop		bx
+	pop		ax
+	iret
---
+++ Lab4c/yaks.s
@@ -0,0 +1,42 @@
+YKEnterMutex:
+	cli				; disable interrupts
+	ret
+
+YKExitMutex:
+	sti				; enable interrupts
+	ret
+
+YKDispatcher:
+	push	cs		; code segment (segment to which IP refers)
+	pushf	    	; push the flags
+	labelDispatch:
+	push	ax
+	push	bx
+	push	cx
+	push	dx
+	push	si		; source index
+	push	di		; destination index
+	push	bp		; base
+	push	es		; extra segment
+	push	ds		; data segment
+
+	; currentTask is first parameter passed, nextTask is 2nd passed
+	; each TCB has 3 ints, 2 chars, and 1 struct pointer (6 things)
+
+	mov bx, [taskSaveCTX]		; save currentTask
+	mov	[bx], sp		; set sp and TCB of currentTask
+		
+	mov bx, [nextTask]		; save nextTask
+	mov sp, [bx]		; restore context of nextTask by getting sp from TB of nextTask
+
+	pop		ds		; pop everything but ip, cs, and flags (reverse order of course)
+	pop		es
+	pop		bp
+	pop		di
+	pop		si
+	pop		dx
+	pop		cx
+	pop		bx
+	pop		ax
+
+	iret			; start next task. iret takes care of ip, cs, and flags
